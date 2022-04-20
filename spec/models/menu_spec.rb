be59e297d8d6a086fb40c1dd0e37f1ff31acbe3a
arceister <imandajagad01@gmail.com>
2022-04-21 05:28:36 +0700

    Test: add model invalid with duplicate testcase for `food` model

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Menu, type: :model do
 
   expect(menu).to be_valid
   end
+
+  it "is invalid with a duplicate name" do
+    menu1 = Menu.create(
+      name: 'Nasi Goreng',
+      description: 'Nasi di goreng',
+      price: 9000.0
+    )
+
+    menu2 = Menu.new(
+      name: 'Nasi Goreng',
+      description: 'Nasi di tumis',
+      price: 9000.0
+    )
+  end
 end
