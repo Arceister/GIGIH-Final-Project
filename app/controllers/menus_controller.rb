69955a4a1005da781d5a8cd86d2cd7ca1c05aef2
arceister <imandajagad01@gmail.com>
2022-04-22 02:31:31 +0700

    Test: passed `PATCH #update` locates requested @menu `testcase`

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -8,6 +8,20 @@ class MenusController < ApplicationController
   def new
   end
 
+  def create
+    menu = Menu.create(menu_parameters)
+
+    redirect_to menus_path
+  end
+
   def edit
+  end
+
+  def update
+    @menu = Menu.find(params[:id])
+  end
+
+  def menu_parameters
+    params.require("menu").permit(:name, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'menus/index'
-  get 'menus/show'
-  get 'menus/new'
-  get 'menus/edit'
+  resources :menus
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
