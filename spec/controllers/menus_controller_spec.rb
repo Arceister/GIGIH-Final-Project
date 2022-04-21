86a919fefc5d5476a5114a27a76d28202f641335
arceister <imandajagad01@gmail.com>
2022-04-22 00:50:54 +0700

    Test: add `PATCH #update` test

---
+++ spec/controllers/menus_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe MenusController do
+  describe MenusController do
+    describe 'PATCH #update' do
+      before :each do
+        @food = create(:food)
+      end
+    end
+  end
+end
