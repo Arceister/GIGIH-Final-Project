e8409a9febfd7818528cae3c16b37609785886b5
arceister <imandajagad01@gmail.com>
2022-04-21 13:00:49 +0700

    Test: fix model invald with desc >150 char testcase for `menu` model

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe Menu, type: :model do
       description: "Nasi di goreng tapi deskripsinya lebih dari 150 karakter. Lorem ipsum dolor sit amet, 
         consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      price: 0.00
+      price: 15000.00
     )
   end
 end
