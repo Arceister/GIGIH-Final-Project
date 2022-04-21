91cd8c511f4f6500d8c9e7203d37833c7047c6a0
arceister <imandajagad01@gmail.com>
2022-04-21 13:04:40 +0700

    Test: passed model invald with desc >150 char testcase for `menu` model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,5 @@
 class Menu < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: {greater_than: 0.01}
+  validates :description, length: {maximum: 150}
 end
