f99b45d79090d894e348a658198d4ea25d45295d
arceister <imandajagad01@gmail.com>
2022-04-22 00:07:27 +0700

    Test: add `menu_category` model valid with more than one category `testcase`

--- spec/models/menu_category_spec.rb
+++ spec/models/menu_category_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe MenuCategory, type: :model do
 
     expect(menu.categories.length).to be >= 1
   end
+
+  it "is valid with more than one category" do
+    c1 = Category.create(name: 'Indonesian')
+    c2 = Category.create(name: 'Fried Food')
+
+    menu = Menu.create(
+      name: 'Nasi Goreng',
+      description: 'Nasi di goreng',
+      price: 9000.0,
+      categories: [c1, c2]
+    )
+  end
 end
