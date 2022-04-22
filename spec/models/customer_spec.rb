45f60e858ae88c15118893bca3319e98ccff3029
arceister <imandajagad01@gmail.com>
2022-04-23 04:25:34 +0700

    Test: add `testcase` invalid email for `customer` model

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Customer, type: :model do
   it "is a valid email format" do
     expect(FactoryBot.build(:customer)).to be_valid
   end
+
+  it "is invalid without correct email format" do
+    
+  end
 end
