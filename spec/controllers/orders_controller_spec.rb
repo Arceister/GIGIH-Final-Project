adb7dfe8d7ba77cc1bae7794024b21c4f3c65a45
arceister <imandajagad01@gmail.com>
2022-04-23 06:25:38 +0700

    Test: add `testcase` save new order `order` controller

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrdersController do
   describe 'POST #create' do
-    
+    it "saves new order in the database" do
+      cust = Customer.create(attributes_for(:customer))
+      order = Order.create(attributes_for(:order, customer: cust))
+    end
   end
 end
