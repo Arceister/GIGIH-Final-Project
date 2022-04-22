94cc323c68bb0f9adfac085075ff058dd3e70f29
arceister <imandajagad01@gmail.com>
2022-04-23 06:13:41 +0700

    Add: generate `orders` controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
+  get 'orders/edit'
   resources :menus
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
