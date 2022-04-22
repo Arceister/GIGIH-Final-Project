9115e17c58b082ded47ba94c76968083bb11e245
arceister <imandajagad01@gmail.com>
2022-04-23 05:30:03 +0700

    Refactor: change relation on `order.rb` and `menu.rb`

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,8 @@
 class Menu < ApplicationRecord
   has_many :menu_categories
+  has_many :order_menus
   has_many :categories, through: :menu_categories
+  has_many :orders, through: :order_menus
 
   validates :name, presence: true, uniqueness: true
   validates :price, numericality: {greater_than: 0.01}
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
+
+  has_many :order_menus
+  has_many :menus, through: :order_menus
 end
